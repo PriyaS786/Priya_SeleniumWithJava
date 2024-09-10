cb4c971c10330cd2a4b435161b85baf90a25499b
PriyaS786 <Priyanka.surnar85@gmail.com>
2024-09-10 22:39:43 +0200

    changes in base package

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -1,9 +1,15 @@
 package base;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class Base {
+    public static WebDriver driver;
 
     public static void main(String[] args) {
-        System.out.println("hello all");
+        WebDriver driver=new ChromeDriver();
+        driver.manage().window().maximize();
+        driver.get("https://bitbucket.org/product/");
 
     }
 }
