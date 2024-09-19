f7da630501d106f3f4f8e9e6790f46b73112216c
PriyaS786 <Priyanka.surnar85@gmail.com>
2024-09-19 14:38:18 +0200

    create new packages

--- src/main/java/base/Base.java
+++ src/main/java/base/Base.java
@@ -1,16 +1,5 @@
 package base;
 
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-
 public class Base {
-    public static WebDriver driver;
 
-
-
-        WebDriver driver=new ChromeDriver();
-        driver.manage().window().maximize();
-        driver.get("https://demo.applitools.com/app.html");
-
-    }
-
+}
